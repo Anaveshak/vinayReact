19a76fbd2a7d8c9c270e2819e6cca3daaa2969ec
Vinay kumar sain <kumarsainvinay15@gmail.com>
2025-02-19 14:40:07 +0530

    Add files via upload

---
+++ dummydata.js
@@ -0,0 +1,217 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Friends",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "VINAY KUMAR SAIN",
+    post: "WEB DESIGNER",
+    design: "UI / UX DESIGNER",
+    desc: "HELLO my name is vinay kumar sain learn in ccsu university that is a practice project for react js"
+  }
+  ]
+
+export const about = [
+  {
+    desc: "Hey, I’m Vinay, a web devlopment . I love exploring Web tecnology and creating portfolio website. With a background in BCA from ccsu, I strive to become a webb devloper. When I’m not working, you’ll find me playing chess.",
+    desc1: "Tech enthusiast | Passionate about IT | Lifelong Learner | Let’s connect and create something amazing!",
+    cover: "./images/vinaay.jpg",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Aashish",
+    desc: "Smart boy cool and dashing",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "dhruv",
+    desc: "inteligent boy",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "anurag",
+    desc: "ramesh",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "3",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "3",
+    title: "PROJECTS COMPLEATED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "4",
+    title: " total friends",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "0",
+    title: "Women intrection",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port2/gif1.gif",
+    name: "aashish",
+    category: "smart",
+    title: "in lead",
+  },
+  {
+    id: 2,
+    cover: "../images/port2/gif3.gif",
+    name: "dhruv",
+    category: "sathi",
+    title: "best frnd",
+  },
+  {
+    id: 3,
+    cover: "../images/port2/gif2.gif",
+    name: "ayushi ji",
+    category: "development",
+    title: "kya kahuu",
+  },
+  {
+    id: 4,
+    cover: "../images/port2/gif4.jpg",
+    name: "anurag",
+    category: "ramesh",
+    title: "sher",
+  },
+  {
+    id: 5,
+    cover: "../images/port2/gif5.gif",
+    name: "pushpa",
+    category: "domst",
+    title: "sher",
+  },
+  {
+    id: 6,
+    cover: "../images/port2/itc.jpg",
+    name: "extra",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "vinay is an incredibly dedicated and skilled professional. Their expertise in react js and commitment to excellence make them a valuable asset to any team.",
+    image: "./images/sarkari.png",
+    name: "Vinay kumar sain",
+    post: "Front End Developer",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/gojo.jpg",
+    name: "Gojo",
+    post: "Back End Developer",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/girlpng.jpg",
+    name: "women",
+    post: "React Developer",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply ",
+    cover: "./images/blog/hapydog.gif",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply ",
+    cover: "./images/blog/hapydog.gif",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply",
+    cover: "./images/blog/hapydog.gif",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "216 ram nagar ambbhedkar road ",
+    text2: "near shagun sweet ghaziabad",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "7982537040",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "kumarsainvinay15@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
